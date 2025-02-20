2328272eac05d76acf86c4a02fa81066d6ec66d8
나상준 <sangjoon1002@naver.com>
2025-02-21 01:48:23 +0900

    유저 정보 엔티티 추가 및 베이스 타임 설정

---
+++ maping/src/main/java/org/maping/maping/model/user/UserInfoJpaEntity.java
@@ -0,0 +1,36 @@
+package org.maping.maping.model.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "USER_INFO_TB")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class UserInfoJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "iconic")
+    private String iconic;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+}
