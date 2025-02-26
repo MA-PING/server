17bf5d8f6c496357ea10b56001cc158ed35ac0bf
seongjuun <beajon0301@naver.com>
2025-02-26 22:53:47 +0900

    넥슨 api 수정

--- maping/src/main/java/org/maping/maping/api/character/service/CharacterServiceImpl.java
+++ maping/src/main/java/org/maping/maping/api/character/service/CharacterServiceImpl.java
@@ -1,20 +1,39 @@
 package org.maping.maping.api.character.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.maping.maping.common.utills.nexon.NEXONUtils;
+import org.maping.maping.common.utills.nexon.dto.character.CharacterDTO;
 import org.maping.maping.common.utills.nexon.dto.character.CharacterInfoDTO;
+import org.maping.maping.model.ocid.OcidJpaEntity;
+import org.maping.maping.repository.ocid.OcidRepository;
 import org.springframework.stereotype.Service;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
 @Service
 public class CharacterServiceImpl implements CharacterService {
 
     private final NEXONUtils nexonUtils;
+    private final OcidRepository ocidRepository;
 
-    public CharacterServiceImpl(NEXONUtils nexonUtils) {
+    public CharacterServiceImpl(NEXONUtils nexonUtils, OcidRepository ocidRepository) {
         this.nexonUtils = nexonUtils;
+        this.ocidRepository = ocidRepository;
     }
 
     public CharacterInfoDTO getCharacterInfo(String characterName) {
+        if(characterName == null || characterName.trim().isEmpty()) {
+            throw new IllegalArgumentException("캐릭터 이름을 입력해주세요.");
+        }
+        CharacterDTO characterDto = nexonUtils.getOcid(characterName);
+        String ocid = characterDto.getOcid();
+        log.info("service ocid: {}", ocid);
 
-        return nexonUtils.getCharacterInfo(characterName);
+        if (ocid == null || ocid.trim().isEmpty()) {
+            throw new IllegalArgumentException("유효하지 않은 ocid입니다.");
+        }
+        return nexonUtils.getCharacterInfo(ocid);
     }
 }
--- maping/src/main/java/org/maping/maping/common/utills/nexon/NEXONUtils.java
+++ maping/src/main/java/org/maping/maping/common/utills/nexon/NEXONUtils.java
@@ -1,6 +1,8 @@
 package org.maping.maping.common.utills.nexon;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.constraints.NotBlank;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.maping.maping.common.enums.expection.ErrorCode;
 import org.maping.maping.common.utills.nexon.dto.character.*;
@@ -26,9 +28,17 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CompletableFuture;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Slf4j
 @RestController
@@ -38,42 +48,58 @@ public class NEXONUtils {
 
     public final String Key;
 
-    public NEXONUtils(@Value("${spring.nexon.key}") String key) {
-        Key = key;
+    public NEXONUtils(@Value("${spring.nexon.key2}") String key) {
+        this.Key = key;
     }
 
     @Operation(summary = "ocid 가져오기", description = "캐릭터 이름을 통해 ocid를 가져오는 API")
-    public CharacterDTO getOcid(String characterName) {
+    public CharacterDTO getOcid(@NonNull String characterName) {
         String apiUrl = "https://open.api.nexon.com/maplestory/v1/id";
-        String fullUrl = UriComponentsBuilder.fromUriString(apiUrl).queryParam("character_name", characterName).build().toUriString();
+        log.info("getOcid: {}", characterName);
+
+        // 캐릭터 이름을 URL 인코딩
+        String encodedCharacterName = URLEncoder.encode(characterName, UTF_8);
+        log.info(encodedCharacterName);
+
+        String fullUrl = UriComponentsBuilder.fromUriString(apiUrl)
+                .queryParam("character_name", characterName)
+                .build()
+                .toUriString();
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("x-nxopen-api-key", Key);
-
-        ResponseEntity<CharacterDTO> responseEntity = new RestTemplate().exchange(fullUrl, HttpMethod.GET, new HttpEntity<>(headers), CharacterDTO.class);
-        return responseEntity.getBody();
+        log.info(Key);
+        log.info(fullUrl);
+        log.info(headers.toString());
+//        ResponseEntity<CharacterDTO> responseEntity = new RestTemplate().exchange(fullUrl, HttpMethod.GET, new HttpEntity<>(headers), CharacterDTO.class);
+//        return responseEntity.getBody();
+        try {
+            ResponseEntity<CharacterDTO> responseEntity = new RestTemplate().exchange(fullUrl, HttpMethod.GET, new HttpEntity<>(headers), CharacterDTO.class);
+            return responseEntity.getBody();
+        } catch (HttpClientErrorException e) {
+            log.error("Error response: {}", e.getResponseBodyAsString());
+            throw e;
+        }
     }
 
     @Operation(summary = "캐릭터 기본 정보 가져오기", description = "ocid를 통해 캐릭터의 기본 정보를 가져오는 API")
-    public CharacterBasicDTO getCharcterBasic(String ocid) {
+    public CharacterBasicDTO getCharacterBasic(@NonNull String ocid) {
         String apiUrl = "https://open.api.nexon.com/maplestory/v1/character/basic";
         String fullUrl = UriComponentsBuilder.fromUriString(apiUrl).queryParam("ocid", ocid).build().toUriString();
 
-        // 헤더 설정
+        log.info("getCharacterBasic: {}", ocid);
+        log.info("fullUrl: {}", fullUrl);
         HttpHeaders headers = new HttpHeaders();
         headers.set("x-nxopen-api-key", Key);
 
-        // API 호출
         ResponseEntity<CharacterBasicDTO> response = new RestTemplate().exchange(fullUrl, HttpMethod.GET, new HttpEntity<>(headers), CharacterBasicDTO.class);
 
-        // 응답 반환
-        return response.getBody(); // API 응답 그대로 반환
+        return response.getBody();
     }
 
     @Operation(summary = "캐릭터 리스트 가져오기", description = "API 키를 통해 캐릭터 리스트를 가져오는 API")
-    public CharacterListDto getCharcterList(String apiKey) {
+    public CharacterListDto getCharacterList(@NotBlank String apiKey) {
         String apiUrl = "https://open.api.nexon.com/maplestory/v1/character/list";
-
         HttpHeaders headers = new HttpHeaders();
         headers.set("x-nxopen-api-key", Key);
 
@@ -82,7 +108,7 @@ public class NEXONUtils {
     }
 
     @Operation(summary = "캐릭터 스탯 가져오기", description = "ocid를 통해 캐릭터의 스탯을 가져오는 API")
-    public CharacterStatDto getCharcterStat(String ocid) {
+    public CharacterStatDto getCharacterStat(@NotBlank String ocid) {
         String apiUrl = "https://open.api.nexon.com/maplestory/v1/character/stat";
         String fullUrl = UriComponentsBuilder.fromUriString(apiUrl).queryParam("ocid", ocid).build().toUriString();
 
@@ -93,7 +119,7 @@ public class NEXONUtils {
     }
 
     @Operation(summary = "캐릭터 하이퍼스탯 가져오기", description = "ocid를 통해 캐릭터의 하이퍼스탯을 가져오는 API")
-    public CharacterHyperStatDTO getCharcterHyperStat(String ocid) {
+    public CharacterHyperStatDTO getCharacterHyperStat(@NotBlank String ocid) {
         String apiUrl = "https://open.api.nexon.com/maplestory/v1/character/hyper-stat";
         String fullUrl = UriComponentsBuilder.fromUriString(apiUrl).queryParam("ocid", ocid).build().toUriString();
 
@@ -105,7 +131,7 @@ public class NEXONUtils {
     }
 
     @Operation(summary = "캐릭터 어빌리티 가져오기", description = "ocid를 통해 캐릭터의 능력치를 가져오는 API")
-    public CharacterAbilityDTO getCharcterAbility(String ocid) {
+    public CharacterAbilityDTO getCharacterAbility(@NotBlank String ocid) {
         String apiUrl = "https://open.api.nexon.com/maplestory/v1/character/ability";
         String fullUrl = UriComponentsBuilder.fromUriString(apiUrl).queryParam("ocid", ocid).build().toUriString();
 
@@ -117,7 +143,7 @@ public class NEXONUtils {
     }
 
     @Operation(summary = "캐릭터 아이템 정보 가져오기", description = "ocid를 통해 캐릭터의 아이템 정보를 가져오는 API")
-    public CharacterItemEquipmentDTO getCharcterItemEquip(String ocid) {
+    public CharacterItemEquipmentDTO getCharacterItemEquip(@NotBlank String ocid) {
         String apiUrl = "https://open.api.nexon.com/maplestory/v1/character/item-equipment";
         String fullUrl = UriComponentsBuilder.fromUriString(apiUrl).queryParam("ocid", ocid).build().toUriString();
 
@@ -128,7 +154,7 @@ public class NEXONUtils {
     }
 
     @Operation(summary = "캐릭터 심볼 정보 가져오기", description = "ocid를 통해 캐릭터의 심볼 정보를 가져오는 API")
-    public CharacterSymbolEquipmentDTO getCharcterSymbolEquipment(String ocid) {
+    public CharacterSymbolEquipmentDTO getCharacterSymbolEquipment(@NotBlank String ocid) {
         String apiUrl = "https://open.api.nexon.com/maplestory/v1/character/symbol-equipment";
         String fullUrl = UriComponentsBuilder.fromUriString(apiUrl).queryParam("ocid", ocid).build().toUriString();
 
@@ -140,7 +166,7 @@ public class NEXONUtils {
     }
 
     @Operation(summary = "캐릭터 스킬 정보 가져오기", description = "ocid를 통해 캐릭터의 스킬 정보를 가져오는 API")
-    public CharacterSkillDTO getCharcterSkill5(String ocid, int skillGrade) {
+    public CharacterSkillDTO getCharacterSkill5(@NotBlank String ocid, int skillGrade) {
         String apiUrl = "https://open.api.nexon.com/maplestory/v1/character/skill";
         String fullUrl = UriComponentsBuilder.fromUriString(apiUrl).queryParam("ocid", ocid).queryParam("character_skill_grade", skillGrade).build().toUriString();
 
@@ -152,7 +178,7 @@ public class NEXONUtils {
     }
 
     @Operation(summary = "캐릭터 링크 스킬 정보 가져오기", description = "ocid를 통해 캐릭터의 링크 스킬 정보를 가져오는 API")
-    public CharacterLinkSkillDTO getCharcterLinkSkill(String ocid) {
+    public CharacterLinkSkillDTO getCharacterLinkSkill(@NotBlank String ocid) {
         String apiUrl = "https://open.api.nexon.com/maplestory/v1/character/link-skill";
         String fullUrl = UriComponentsBuilder.fromUriString(apiUrl).queryParam("ocid", ocid).build().toUriString();
 
@@ -164,7 +190,7 @@ public class NEXONUtils {
     }
 
     @Operation(summary = "캐릭터 V매트릭스 정보 가져오기", description = "ocid를 통해 캐릭터의 V매트릭스 정보를 가져오는 API")
-    public CharacterVMatrixDTO getCharcterVmatrix(String ocid) {
+    public CharacterVMatrixDTO getCharacterVmatrix(@NotBlank String ocid) {
         String apiUrl = "https://open.api.nexon.com/maplestory/v1/character/vmatrix";
         String fullUrl = UriComponentsBuilder.fromUriString(apiUrl).queryParam("ocid", ocid).build().toUriString();
 
@@ -176,7 +202,7 @@ public class NEXONUtils {
     }
 
     @Operation(summary = "캐릭터 HEXA 매트릭스 정보 가져오기", description = "ocid를 통해 캐릭터의 HEXA 매트릭스 정보를 가져오는 API")
-    public CharacterHexaMatrixDTO getCharcterHexamatrix(String ocid) {
+    public CharacterHexaMatrixDTO getCharacterHexamatrix(@NotBlank String ocid) {
         String apiUrl = "https://open.api.nexon.com/maplestory/v1/character/hexamatrix";
         String fullUrl = UriComponentsBuilder.fromUriString(apiUrl).queryParam("ocid", ocid).build().toUriString();
 
@@ -188,7 +214,7 @@ public class NEXONUtils {
     }
 
     @Operation(summary = "캐릭터 HEXA 매트릭스 스탯 정보 가져오기", description = "ocid를 통해 캐릭터의 HEXA 매트릭스 스탯 정보를 가져오는 API")
-    public CharacterHexaMatrixStatDTO getCharcterHexamatrixStat(String ocid) {
+    public CharacterHexaMatrixStatDTO getCharacterHexamatrixStat(@NotBlank String ocid) {
         String apiUrl = "https://open.api.nexon.com/maplestory/v1/character/hexamatrix-stat";
         String fullUrl = UriComponentsBuilder.fromUriString(apiUrl).queryParam("ocid", ocid).queryParam("hexa_matrix_id", 1).build().toUriString();
 
@@ -200,7 +226,7 @@ public class NEXONUtils {
     }
 
     @Operation(summary = "캐릭터 유니온 정보 가져오기", description = "ocid를 통해 캐릭터의 유니온 정보를 가져오는 API")
-    public UnionDTO getUnion(String ocid) {
+    public UnionDTO getUnion(@NotBlank String ocid) {
         String apiUrl = "https://open.api.nexon.com/maplestory/v1/user/union";
         String fullUrl = UriComponentsBuilder.fromUriString(apiUrl).queryParam("ocid", ocid).queryParam("union_id", 1).build().toUriString();
 
@@ -212,7 +238,7 @@ public class NEXONUtils {
     }
 
     @Operation(summary = "캐릭터 유니온 레이더 정보 가져오기", description = "ocid를 통해 캐릭터의 유니온 레이더 정보를 가져오는 API")
-    public UnionRaiderDTO getUnionRaider(String ocid) {
+    public UnionRaiderDTO getUnionRaider(@NotBlank String ocid) {
         String apiUrl = "https://open.api.nexon.com/maplestory/v1/user/union-raider";
         String fullUrl = UriComponentsBuilder.fromUriString(apiUrl).queryParam("ocid", ocid).queryParam("union_id", 1).queryParam("raider_id", 1).build().toUriString();
 
@@ -224,7 +250,7 @@ public class NEXONUtils {
     }
 
     @Operation(summary = "캐릭터 유니온 아티팩트 정보 가져오기", description = "ocid를 통해 캐릭터의 유니온 아티팩트 정보를 가져오는 API")
-    public UnionArtifactDTO getUnionArtifact(String ocid) {
+    public UnionArtifactDTO getUnionArtifact(@NotBlank String ocid) {
         String apiUrl = "https://open.api.nexon.com/maplestory/v1/user/union-artifact";
         String fullUrl = UriComponentsBuilder.fromUriString(apiUrl).queryParam("ocid", ocid).queryParam("union_id", 1).queryParam("artifact_id", 1).build().toUriString();
 
@@ -302,28 +328,47 @@ public class NEXONUtils {
         return response.getBody();
     }
 
-    @Operation(summary = "캐릭터 정보 가져오기", description = "캐릭터 이름을 통해 캐릭터의 기본 정보를 가져오는 API")
-    public CharacterInfoDTO getCharacterInfo(String characterName) {
+    @Operation(summary = "캐릭터 정보 첫페이지 가져오기", description = "캐릭터 이름을 통해 캐릭터의 기본 정보를 가져오는 API")
+    public CharacterInfoDTO getCharacterInfo(String ocid) {
         CharacterInfoDTO characterInfo = new CharacterInfoDTO();
-        CharacterDTO characterDto = getOcid(characterName);
-        log.info("ocid: {}", characterDto.getOcid());
-        String ocid = characterDto.getOcid();
+        log.info("getCharacterInfo: {}", ocid);
+
+        // 비동기 호출을 사용하여 각 정보를 가져옵니다.
+        CompletableFuture<CharacterBasicDTO> basicFuture = CompletableFuture.supplyAsync(() -> getCharacterBasic(ocid));
+        CompletableFuture<CharacterStatDto> statFuture = CompletableFuture.supplyAsync(() -> getCharacterStat(ocid));
+        CompletableFuture<CharacterHyperStatDTO> hyperStatFuture = CompletableFuture.supplyAsync(() -> getCharacterHyperStat(ocid));
+        CompletableFuture<CharacterAbilityDTO> abilityFuture = CompletableFuture.supplyAsync(() -> getCharacterAbility(ocid));
+        CompletableFuture<CharacterItemEquipmentDTO> itemEquipmentFuture = CompletableFuture.supplyAsync(() -> getCharacterItemEquip(ocid));
+        CompletableFuture<CharacterSymbolEquipmentDTO> symbolEquipmentFuture = CompletableFuture.supplyAsync(() -> getCharacterSymbolEquipment(ocid));
+        CompletableFuture<CharacterSkillDTO> skill5Future = CompletableFuture.supplyAsync(() -> getCharacterSkill5(ocid, 5));
+        CompletableFuture<CharacterSkillDTO> skill6Future = CompletableFuture.supplyAsync(() -> getCharacterSkill5(ocid, 6));
+        CompletableFuture<CharacterLinkSkillDTO> linkSkillFuture = CompletableFuture.supplyAsync(() -> getCharacterLinkSkill(ocid));
+        CompletableFuture<CharacterVMatrixDTO> vMatrixFuture = CompletableFuture.supplyAsync(() -> getCharacterVmatrix(ocid));
+        CompletableFuture<CharacterHexaMatrixDTO> hexaMatrixFuture = CompletableFuture.supplyAsync(() -> getCharacterHexamatrix(ocid));
+        CompletableFuture<CharacterHexaMatrixStatDTO> hexaMatrixStatFuture = CompletableFuture.supplyAsync(() -> getCharacterHexamatrixStat(ocid));
+        CompletableFuture<UnionDTO> unionFuture = CompletableFuture.supplyAsync(() -> getUnion(ocid));
+        CompletableFuture<UnionRaiderDTO> unionRaiderFuture = CompletableFuture.supplyAsync(() -> getUnionRaider(ocid));
+        CompletableFuture<UnionArtifactDTO> unionArtifactFuture = CompletableFuture.supplyAsync(() -> getUnionArtifact(ocid));
+
+        // 모든 비동기 호출의 결과를 기다립니다.
         characterInfo.setOcid(ocid);
-        characterInfo.setBasic(getCharcterBasic(ocid));
-        characterInfo.setStat(getCharcterStat(ocid));
-        characterInfo.setHyperStat(getCharcterHyperStat(ocid));
-        characterInfo.setAbility(getCharcterAbility(ocid));
-        characterInfo.setItemEquipment(getCharcterItemEquip(ocid));
-        characterInfo.setSymbolEquipment(getCharcterSymbolEquipment(ocid));
-        characterInfo.setSkill5(getCharcterSkill5(ocid, 5));
-        characterInfo.setSkill6(getCharcterSkill5(ocid, 6));
-        characterInfo.setLinkSkill(getCharcterLinkSkill(ocid));
-        characterInfo.setVMatrix(getCharcterVmatrix(ocid));
-        characterInfo.setHexaMatrix(getCharcterHexamatrix(ocid));
-        characterInfo.setHexaMatrixStat(getCharcterHexamatrixStat(ocid));
-        characterInfo.setUnion(getUnion(ocid));
-        characterInfo.setUnionRaider(getUnionRaider(ocid));
-        characterInfo.setUnionArtifact(getUnionArtifact(ocid));
+        characterInfo.setBasic(basicFuture.join());
+        characterInfo.setStat(statFuture.join());
+        characterInfo.setHyperStat(hyperStatFuture.join());
+        characterInfo.setAbility(abilityFuture.join());
+        characterInfo.setItemEquipment(itemEquipmentFuture.join());
+        characterInfo.setSymbolEquipment(symbolEquipmentFuture.join());
+        characterInfo.setSkill5(skill5Future.join());
+        characterInfo.setSkill6(skill6Future.join());
+        characterInfo.setLinkSkill(linkSkillFuture.join());
+        characterInfo.setVMatrix(vMatrixFuture.join());
+        characterInfo.setHexaMatrix(hexaMatrixFuture.join());
+        characterInfo.setHexaMatrixStat(hexaMatrixStatFuture.join());
+        characterInfo.setUnion(unionFuture.join());
+        characterInfo.setUnionRaider(unionRaiderFuture.join());
+        characterInfo.setUnionArtifact(unionArtifactFuture.join());
+
         return characterInfo;
     }
+
 }
